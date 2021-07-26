650cce64c3378e284ed1412a1e202286e8c3dfb9
fede2s <fede.2santos_96@hotmail.com>
2021-07-26 18:19:14 +0000

    QUnit - Integracion con OPA5

---
+++ Invoices/SAPUI5/webapp/test/integration/NavigationJourney.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+//@ts-nocheck
+sap.ui.define([
+    "fiori_ini/SAPUI5/localService/mockserver",
+    "sap/ui/test/opaQunit",
+    "./pages/HelloPanel"
+], 
+/**
+ * 
+ * @param {typeof sap.ui.test.opaQunit} opaQunit 
+ */
+function (mockserver, opaQunit){
+
+    QUnit.module("Navigation");
+
+    opaQunit("Should open the Hello Dialog", function(Given, When, Then){
+
+        //Initialize the mock server
+        mockserver.init();
+
+        //Arrangements
+        Given.iStartMyUIComponent({
+            componentConfig: {
+                name: "fiori_ini.SAPUI5"
+            }
+        });
+
+        //Actions
+        When.onTheAppPagge.iSayHelloDialogButton();
+
+        //Assertions
+        Then.onTheAppPage.iSeeTheHelloDialog();
+
+        //Cleanup
+        Then.iTeardownMyApp();
+
+    });
+});
