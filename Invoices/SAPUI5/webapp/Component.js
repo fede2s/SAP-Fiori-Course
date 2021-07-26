aa0a098b5356f17305fad3a5c917111d8c1546e2
fede2s <fede.2santos_96@hotmail.com>
2021-07-26 20:20:34 +0000

    Enrutamiento - Inicializacion en el Componente

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -30,6 +30,9 @@ sap.ui.define([
 
                 this._helloDialog = new HelloDialog(this.getRootControl());
 
+                //Create the views based in url/hash
+                this.getRouter().initialize();
+
             },
 
             exit : function () {
