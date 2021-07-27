ea576c692c3818a6e264f5b3bb63ab83c3ceda5e
fede2s <fede.2santos_96@hotmail.com>
2021-07-27 14:47:07 +0000

    Metadatos - Definicion

--- Invoices/SAPUI5/webapp/control/ProductRating.js
+++ Invoices/SAPUI5/webapp/control/ProductRating.js
@@ -14,6 +14,35 @@ sap.ui.define([
 
             metadata: {
 
+                properties: {
+                    value: { type: "float", defaultValue: 0 }
+                },
+
+                aggregations: {
+                    _rating: {
+                        type: "sap.m.RatingIndicator",
+                        multiple: false,
+                        visibility: "hidden"
+                    },
+                    _label : {
+                        type: "sap.m.Label",
+                        multiple : false,
+                        visibility: "hidden"
+                    },
+                    _button: {
+                        type: "sap.m.Button",
+                        multiple: false,
+                        visibility: "hidden"
+                    },
+
+                    events: {
+                        change : {
+                            parameters : {
+                                value: { type : "int" }                                
+                            }
+                        }
+                    }
+                }
             },
 
             init : function () {
