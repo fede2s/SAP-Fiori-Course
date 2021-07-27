aa7d1b1027b0321fe34ab2b4189c6903aa37aac3
fede2s <fede.2santos_96@hotmail.com>
2021-07-27 12:46:50 +0000

    Navegacion con parametros - Recibo

--- Invoices/SAPUI5/webapp/controller/Details.controller.js
+++ Invoices/SAPUI5/webapp/controller/Details.controller.js
@@ -10,8 +10,16 @@ sap.ui.define([
 
         return Controller.extend("fiori_ini.SAPUI5.controller.Details", {
 
-            onInit: function (){
+            _onObjectMatch : function (oEvent) {
+                this.getView().bindElement({
+                    path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+                    model : "northwind"
+                });
+            },
 
+            onInit: function (){
+                const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("Details").attachPatternMatched(this._onObjectMatch, this);
             }
 
         });
