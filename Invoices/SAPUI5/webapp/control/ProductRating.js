37dfc217d33bf2327801d55afa551841380cc440
fede2s <fede.2santos_96@hotmail.com>
2021-07-27 13:52:43 +0000

    Creacion

---
+++ Invoices/SAPUI5/webapp/control/ProductRating.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+//@ts-nocheck
+sap.ui.define([
+    'sap/ui/core/Control'
+],
+/**
+ * @param {typeof sap.ui.core.Control} Control
+ */
+    function (Control) {
+
+        'use strict';
+
+        return Control.extend("fiori_ini.SAPUI5.control.ProductRating", {
+
+            metadata: {
+
+            },
+
+            init : function () {
+
+            },
+
+            renderer: function (oRm, oControl) {
+
+            }
+        });
+    });
